ff46cb313f63f2f7c7b9e258688dda6d62744810
Vanessita <vanessita.torrico@gmail.com>
2024-05-06 14:15:13 +0200

    order by date desc

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -4,4 +4,4 @@ round(sum (turnover),2) as daily_turnover,
 round(sum(purchase_cost),2) as purchase_cost
  FROM `atomic-affinity-420409.cours14.gwz_sales` 
 group by date_date
-order by date_date
+order by date_date desc
